f0ff09ab931834a9a5d2ce59806641c07a19ab29
Roy Bailey <roybaileybiz@gmail.com>
2015-03-26 09:23:33 +0000

    additional nashorn tests and tidy up

--- src/test/java/me/roybailey/research/nashorn/NashornTest.java
+++ src/test/java/me/roybailey/research/nashorn/NashornTest.java
@@ -1,14 +1,12 @@
 package me.roybailey.research.nashorn;
 
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
-import org.junit.After;
-import org.junit.Test;
 import me.roybailey.research.lambda.LambdaTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import javax.script.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -18,69 +16,108 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by roybailey on 18/04/2014.
+ * Set of Nashorn tests to show interoperability of Java8 and Nashorn Javascript.
  */
 public class NashornTest {
 
-    public static String jsWillCallThis(String name) {
-        System.out.println("Called from javascript to print in Java:  " + name);
+    private static void LOG(Object message) {
+        System.out.println("JAVA: " + message);
+    }
+
+    public static String nashornWillCallThis(String name) {
+        LOG("Called from javascript to print in Java:  " + name);
         return "returned from java to print from javascript";
     }
 
-    public static void jsTypesInJava(Object object) {
-        System.out.println("javascript called java with object type: " + object.getClass());
+    public static void nashornTypesInJava(Object object) {
+        LOG("javascript called java with object type: " + object.getClass());
     }
+
+    String JAVASCRIPT_TEST_PARTNER = "src/test/java/"
+            + this.getClass().getPackage().getName().replace(".", File.separator)
+            + "/" + this.getClass().getSimpleName() + ".js";
+    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+    Invocable invocable = (Invocable) engine;
 
     @After
     public void finish() throws InterruptedException {
+        // allow console logging to finish
         Thread.sleep(100);
     }
 
     @Test
     public void testNashorn() throws ScriptException {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         engine.eval("print('Hello World!');");
     }
 
     @Test
-    public void testNashornFile() throws FileNotFoundException, ScriptException, NoSuchMethodException {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-        engine.eval(new FileReader("src/test/java/" + this.getClass().getPackage().getName().replace(".", File.separator) + "/NashornTest.js"));
+    public void testHelloNashorn() throws FileNotFoundException, ScriptException, NoSuchMethodException {
+        engine.eval(new FileReader(JAVASCRIPT_TEST_PARTNER));
+        Object result = invocable.invokeFunction("helloNashornFromJava", "Peter Parker");
+        LOG(result);
+        LOG(result.getClass());
+    }
 
-        Invocable invocable = (Invocable) engine;
+    @Test
+    public void testJavaTypesInNashorn() throws FileNotFoundException, ScriptException, NoSuchMethodException {
+        engine.eval(new FileReader(JAVASCRIPT_TEST_PARTNER));
 
-        Object result = invocable.invokeFunction("fun1", "Peter Parker");
-        System.out.println(result);
-        System.out.println(result.getClass());
-
-        invocable.invokeFunction("fun2", new Date());
+        invocable.invokeFunction("javaTypesInNashorn", new Date());
         // [object java.markdown.Date]
 
-        invocable.invokeFunction("fun2", LocalDateTime.now());
+        invocable.invokeFunction("javaTypesInNashorn", LocalDateTime.now());
         // [object java.time.LocalDateTime]
 
-        invocable.invokeFunction("fun2", new LambdaTest.Person("J", 5));
+        invocable.invokeFunction("javaTypesInNashorn", new LambdaTest.Person("J", 5));
         // [me.roybailey.research.lambda.LambdaTest$Person]
 
-        Map<String,Object> mapData = new HashMap<>();
-        mapData.put("testString","Hello");
-        mapData.put("testNumber",105);
-        invocable.invokeFunction("fun2", mapData);
+        Map<String, Object> mapData = new HashMap<>();
+        mapData.put("testString", "Hello");
+        mapData.put("testNumber", 105);
+        invocable.invokeFunction("javaTypesInNashorn", mapData);
         // [java.util.HashMap]
 
+    }
+
+    @Test
+    public void testThereAndBackAgain() throws FileNotFoundException, ScriptException, NoSuchMethodException {
+        engine.eval(new FileReader(JAVASCRIPT_TEST_PARTNER));
         // call JS and have it call my object method to get some data
         invocable.invokeFunction("thereAndBackAgain", this);
     }
 
-    public Map<String,Object> thereAndBackAgain() {
-        Map<String,Object> mapData = new HashMap<>();
-        mapData.put("StringValue","There And Back Again Worked!");
-        mapData.put("NumberValue",105);
+    public Map<String, Object> thereAndBackAgainJavaObject() {
+        Map<String, Object> mapData = new HashMap<>();
+        mapData.put("StringValue", "There And Back Again Worked!");
+        mapData.put("NumberValue", 105);
         return mapData;
     }
 
     public void thereAndBackAgainScriptObject(ScriptObjectMirror mapData) {
-        mapData.put("StringValue","There And Back Again Worked!");
-        mapData.put("NumberValue",105);
+        mapData.put("StringValue", "There And Back Again Worked!");
+        mapData.put("NumberValue", 105);
+    }
+
+    @Test
+    public void testGlobalDependencyInjection() throws FileNotFoundException, ScriptException, NoSuchMethodException {
+        engine.eval(new FileReader(JAVASCRIPT_TEST_PARTNER));
+
+        // Here we are trying to inject a global service and test whether our
+        // JavaScript can access it as a global variable after startup injection
+        Map<String, Object> mapDependencyInjection = new HashMap<>();
+        mapDependencyInjection.put("service", new Runnable() {
+            @Override
+            public void run() {
+                LOG("This is Java called from Nashorn through a global variable dependency injected object");
+            }
+        });
+        mapDependencyInjection.put("globalNumber", 75);
+        Bindings bindings = new SimpleBindings();
+        bindings.put("ioc", mapDependencyInjection);
+
+        engine.eval("var global = this");
+        engine.setBindings(bindings, ScriptContext.GLOBAL_SCOPE);
+
+        invocable.invokeFunction("testGlobalDependencyInjection");
     }
 }
--- src/test/java/me/roybailey/research/nashorn/NashornTest.js
+++ src/test/java/me/roybailey/research/nashorn/NashornTest.js
@@ -1,48 +1,57 @@
-var fun1 = function(name) {
-    print('Hi there from Javascript, ' + name);
-    return "greetings from javascript";
+var LOG = function(message) {
+    print("NASHORN: " + message);
 };
 
-var fun2 = function (object) {
-    print("JS Class Definition: " + Object.prototype.toString.call(object));
-    print("JSON: " + JSON.stringify(object));
+var helloNashornFromJava = function(name) {
+    LOG('Hi there from Javascript, ' + name);
+    return "greetings from nashorn!";
+};
+
+var javaTypesInNashorn = function (object) {
+    LOG("JS Class Definition: " + Object.prototype.toString.call(object));
+    LOG("JSON: " + JSON.stringify(object));
 };
 
 var thereAndBackAgain = function (object) {
-    print("JS Class Definition: " + Object.prototype.toString.call(object));
-    var javadata = object.thereAndBackAgain();
-    print("JS called Java object instance for return data: " + javadata.StringValue);
+    LOG("JS Class Definition: " + Object.prototype.toString.call(object));
+    var javadata = object.thereAndBackAgainJavaObject();
+    LOG("JS called Java object instance for return data: " + javadata.StringValue);
     var jsdata = {};
     object.thereAndBackAgainScriptObject(jsdata);
-    print("JS called Java object instance for script data: " + JSON.stringify(jsdata));
+    LOG("JS called Java object instance for script data: " + JSON.stringify(jsdata));
 };
 
+var testGlobalDependencyInjection = function() {
+    LOG("ioc=" +ioc);
+    LOG("ioc.globalNumber=" +ioc.globalNumber);
+    ioc.service.run();
+};
 
 var MyJavaClass = Java.type('me.roybailey.research.nashorn.NashornTest');
 
-var result = MyJavaClass.jsWillCallThis('John Doe');
-print(result);
+var result = MyJavaClass.nashornWillCallThis('Nashorn test script evaluated');
+LOG(result);
 
-MyJavaClass.jsTypesInJava(123);
+MyJavaClass.nashornTypesInJava(123);
 // class java.lang.Integer
 
-MyJavaClass.jsTypesInJava(49.99);
+MyJavaClass.nashornTypesInJava(49.99);
 // class java.lang.Double
 
-MyJavaClass.jsTypesInJava(true);
+MyJavaClass.nashornTypesInJava(true);
 // class java.lang.Boolean
 
-MyJavaClass.jsTypesInJava("hi there")
+MyJavaClass.nashornTypesInJava("hi there")
 // class java.lang.String
 
-MyJavaClass.jsTypesInJava(new Number(23));
+MyJavaClass.nashornTypesInJava(new Number(23));
 // class jdk.nashorn.internal.objects.NativeNumber
 
-MyJavaClass.jsTypesInJava(new Date());
+MyJavaClass.nashornTypesInJava(new Date());
 // class jdk.nashorn.internal.objects.NativeDate
 
-MyJavaClass.jsTypesInJava(new RegExp());
+MyJavaClass.nashornTypesInJava(new RegExp());
 // class jdk.nashorn.internal.objects.NativeRegExp
 
-MyJavaClass.jsTypesInJava({foo: 'bar'});
+MyJavaClass.nashornTypesInJava({foo: 'bar'});
 // class jdk.nashorn.internal.scripts.JO4
